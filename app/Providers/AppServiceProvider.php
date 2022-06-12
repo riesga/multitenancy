a6fec0f7c412db4b13c029766ac251bb2951e321
Ricardo E <riesga@gmail.com>
2022-06-12 16:30:42 -0500

    Agregando el paquete passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Passport::routes(null, ['middleware' => [
+            'universal',
+            InitializeTenancyByDomain::class
+        ]]);
     }
 
     /**
